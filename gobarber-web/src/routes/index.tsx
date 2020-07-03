b24ae05440077bc9d7303bc962d181350cf15bb7
Gabriel Cunha <gn_cunha@hotmail.com>
2020-07-03 13:03:51 -0300

    Rotas Autenticadas

--- gobarber-web/src/routes/index.tsx
+++ gobarber-web/src/routes/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
 
 import SignIn from '../pages/SiginIn';
 import SignUp from '../pages/SiginUp';
 
-const Routes = () => {
+import Dashboard from '../pages/Dashboard';
+
+const Routes: React.FC = () => {
   return (
     <Switch>
       <Route component={SignIn} path="/" exact />
       <Route component={SignUp} path="/signup" />
+
+      <Route path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
   );
 };
