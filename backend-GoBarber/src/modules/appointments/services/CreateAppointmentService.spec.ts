ed5917523f2af01106aaf0589fdae6678f0c9378
Gabriel Cunha <gn_cunha@hotmail.com>
2020-07-16 12:15:42 -0300

    Criando Primeiro Teste

--- backend-GoBarber/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend-GoBarber/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,10 +1,17 @@
-import CreateAppointmentService from './CreateAppointmentService';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository'
+import CreateAppointmentService from './CreateAppointmentService'
 
 describe('CreateAppointment',()=>{
-    it('should be able to create a new appointment',()=>{
-        expect(1+2).toBe(3)
-    });
-    it('should not be able to create two appointments on the same time',()=>{
-        expect(1+2).toBe(3);
-    });
-});
+    it('should be able to create a new appointment', async ()=>{
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+        const createAppointments = new CreateAppointmentService(fakeAppointmentsRepository);
+
+        const appointment = await createAppointments.execute({
+            date:new Date(),
+            provider_id:'123123'
+        });
+          expect(appointment).toHaveProperty('id');
+          expect(appointment.provider_id).toBe('123123');
+    })
+
+})
