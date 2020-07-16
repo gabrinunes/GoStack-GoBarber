28fcfd2ecbae2af280e389d24716db66b42bce56
Gabriel Cunha <gn_cunha@hotmail.com>
2020-07-16 18:55:03 -0300

    Criando Teste de Agendamento

--- backend-GoBarber/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend-GoBarber/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,12 +1,14 @@
+import AppError from '@shared/errors/AppError'
+
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository'
 import CreateAppointmentService from './CreateAppointmentService'
 
 describe('CreateAppointment',()=>{
     it('should be able to create a new appointment', async ()=>{
         const fakeAppointmentsRepository = new FakeAppointmentsRepository();
-        const createAppointments = new CreateAppointmentService(fakeAppointmentsRepository);
+        const createAppointment = new CreateAppointmentService(fakeAppointmentsRepository);
 
-        const appointment = await createAppointments.execute({
+        const appointment = await createAppointment.execute({
             date:new Date(),
             provider_id:'123123'
         });
@@ -14,4 +16,19 @@ describe('CreateAppointment',()=>{
           expect(appointment.provider_id).toBe('123123');
     })
 
+    it('should not be able to create two appointments on the same time',async()=>{
+        const fakeAppointmentsRepository = new  FakeAppointmentsRepository();
+        const createAppointment = new CreateAppointmentService(fakeAppointmentsRepository);
+
+        const appointmentDate = new Date(2020, 4, 10, 11);
+
+        await createAppointment.execute({
+            date:appointmentDate,
+            provider_id:'123123'
+        })
+        expect(createAppointment.execute({
+            date:appointmentDate,
+            provider_id:'123123',
+        })).rejects.toBeInstanceOf(AppError);
+    })
 })
