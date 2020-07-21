aa2f153e50958765578ad3b8230e9b88567a56e9
Gabriel Cunha <gn_cunha@hotmail.com>
2020-07-21 16:04:04 -0300

    Salvando Tokens no banco

--- backend-GoBarber/src/shared/container/index.ts
+++ backend-GoBarber/src/shared/container/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-unresolved */
 
-import '@modules/users/providers'
+import '@modules/users/providers';
 import './providers';
 
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
@@ -9,7 +9,8 @@ import IAppointmentsRepository from '@modules/appointments/repositories/IAppoint
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
-
+import IUserTokensRepository from '@modules/users/repositories/IUsersTokenRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
 import { container } from 'tsyringe';
 
@@ -21,4 +22,9 @@ container.registerSingleton<IAppointmentsRepository>(
 container.registerSingleton<IUsersRepository>(
     'UsersRepository',
     UsersRepository,
+);
+
+container.registerSingleton<IUserTokensRepository>(
+    'UserTokensRepository',
+    UserTokensRepository,
 );
