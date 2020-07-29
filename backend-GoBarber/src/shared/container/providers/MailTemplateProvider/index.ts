2421a8c0cf8b0c70a887587d979ba321508072fc
Gabriel Cunha <gn_cunha@hotmail.com>
2020-07-29 11:20:51 -0300

    refatorando Container

---
+++ backend-GoBarber/src/shared/container/providers/MailTemplateProvider/index.ts
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-unresolved */
+import { container } from 'tsyringe';
+import mailConfig from '@config/mail';
+
+import IMailTemplateProvider from './models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvider from './implementations/HandlebarsMailTemplateProvider';
+
+const providers = {
+    handlebars: HandlebarsMailTemplateProvider,
+};
+
+container.registerSingleton<IMailTemplateProvider>(
+    'MailTemplateProvider',
+    providers.handlebars,
+);
---
+++ backend-GoBarber/src/shared/container/providers/StorageProvider/index.ts
@@ -0,0 +1,14 @@
+/* eslint-disable import/extensions */
+import { container } from 'tsyringe';
+
+import IStorageProvider from './models/IStorageProvider';
+import DiskStorageProvider from './implementations/DiskStorageProvider';
+
+const providers = {
+    diskStorage: DiskStorageProvider,
+};
+
+container.registerSingleton<IStorageProvider>(
+    'StorageProvider',
+    providers.diskStorage,
+);
