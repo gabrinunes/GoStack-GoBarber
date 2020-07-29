db5d928dcc6f2d20801e8c7d1e9bca5aab9d8418
Gabriel Cunha <gn_cunha@hotmail.com>
2020-07-29 18:42:07 -0300

    Criando Cache lista de providers

--- backend-GoBarber/src/modules/appointments/services/ListProvidersService.ts
+++ backend-GoBarber/src/modules/appointments/services/ListProvidersService.ts
@@ -1,5 +1,7 @@
 /* eslint-disable import/no-unresolved */
 import { injectable, inject } from 'tsyringe';
+
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
@@ -12,14 +14,27 @@ class ListProvidersService {
     constructor(
         @inject('UsersRepository')
         private usersRepository: IUsersRepository,
+
+        @inject('CacheProvider')
+        private cacheProvider: ICacheProvider,
     ) {}
 
     public async execute({ user_id }: IRequest): Promise<User[]> {
-        const user = await this.usersRepository.findAllProviders({
-            except_user_id: user_id,
-        });
+        let users = await this.cacheProvider.recover<User[]>(
+            `providers-list:${user_id}`,
+        );
 
-        return user;
+        if (!users) {
+            users = await this.usersRepository.findAllProviders({
+                except_user_id: user_id,
+            });
+
+            console.log('A query no banco foi feita');
+
+            await this.cacheProvider.save(`providers-list:${user_id}`, users);
+        }
+
+        return users;
     }
 }
 
