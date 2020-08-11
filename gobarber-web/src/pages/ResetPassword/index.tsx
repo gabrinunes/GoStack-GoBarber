ef20c7bb70dbaadd484ed4c2dfcb9fe0ffd8b467
Gabriel Cunha <gn_cunha@hotmail.com>
2020-08-11 19:36:49 -0300

    Redefinição de Senha

--- gobarber-web/src/pages/ResetPassword/index.tsx
+++ gobarber-web/src/pages/ResetPassword/index.tsx
@@ -1,12 +1,12 @@
+/* eslint-disable camelcase */
 /* eslint-disable no-unused-expressions */
 import React, { useRef, useCallback } from 'react';
-import { Link } from 'react-router-dom';
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { useHistory, useLocation } from 'react-router-dom';
+import { FiLock } from 'react-icons/fi';
 import * as Yup from 'yup';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 
-import { useAuth } from '../../hooks/auth';
 import { useToast } from '../../hooks/toast';
 import getValidationErrors from '../../utils/getValidationsErrors';
 
@@ -16,37 +16,52 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import { Container, Background, Content, AnimationContainer } from './styles';
+import api from '../../services/api';
 
-interface SignInFormData {
-  email: string;
+interface ResetPasswordFormData {
   password: string;
+  password_confirmation: string;
 }
 
-const SignIn: React.FC = () => {
+const ResetPassword: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { signIn } = useAuth();
   const { addToast } = useToast();
+  const location = useLocation();
+
+  const history = useHistory();
 
   const handleSubmit = useCallback(
-    async (data: SignInFormData) => {
+    async (data: ResetPasswordFormData) => {
       try {
         formRef.current?.setErrors({});
 
         const schema = Yup.object().shape({
-          email: Yup.string()
-            .required('E-mail obrigatório')
-            .email('Digite um e-mail válido'),
           password: Yup.string().required('Senha obrigatória'),
+          password_confirmation: Yup.string().oneOf([
+            Yup.ref('password'),
+            'Confirmação incorreta',
+          ]),
         });
+
         await schema.validate(data, {
           abortEarly: false,
         });
 
-        await signIn({
-          email: data.email,
-          password: data.password,
+        const { password, password_confirmation } = data;
+        const token = location.search.replace('?token=', '');
+
+        await api.post('password/reset', {
+          password,
+          password_confirmation,
+          token,
         });
+
+        if (!token) {
+          throw new Error();
+        }
+
+        history.push('/');
       } catch (error) {
         if (error instanceof Yup.ValidationError) {
           const errors = getValidationErrors(error);
@@ -57,13 +72,14 @@ const SignIn: React.FC = () => {
         }
 
         addToast({
-          title: 'Erro na autenticação',
-          description: 'Ocorreu um erro ao fazer login, cheque as credenciais.',
+          title: 'Erro ao resetar senha',
+          description:
+            'Ocorreu um erro ao resetar sua senha, cheque as credenciais.',
           type: 'error',
         });
       }
     },
-    [signIn, addToast],
+    [addToast, history, location.search],
   );
   return (
     <Container>
@@ -71,30 +87,28 @@ const SignIn: React.FC = () => {
         <AnimationContainer>
           <img src={logoImg} alt="GoBarber" />
           <Form ref={formRef} onSubmit={handleSubmit}>
-            <h1>Faça seu logon</h1>
-
-            <Input name="email" icon={FiMail} placeholder="Email" />
+            <h1>Resetar Senha</h1>
 
             <Input
               name="password"
               icon={FiLock}
               type="password"
-              placeholder="Senha"
+              placeholder="Nova Senha"
             />
 
-            <Button type="submit">Entrar</Button>
+            <Input
+              name="password_confirmation"
+              icon={FiLock}
+              type="password"
+              placeholder="Confirmação da senha "
+            />
 
-            <Link to="forgot-password">Esqueci minha senha</Link>
+            <Button type="submit">Alterar Senha</Button>
           </Form>
-
-          <Link to="/signup">
-            <FiLogIn />
-            Criar Conta
-          </Link>
         </AnimationContainer>
       </Content>
       <Background />
     </Container>
   );
 };
-export default SignIn;
+export default ResetPassword;
--- gobarber-web/src/routes/index.tsx
+++ gobarber-web/src/routes/index.tsx
@@ -6,6 +6,7 @@ import Route from './Route';
 import SignIn from '../pages/SiginIn';
 import SignUp from '../pages/SiginUp';
 import ForgotPasswrod from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
 
 import Dashboard from '../pages/Dashboard';
 
@@ -15,6 +16,7 @@ const Routes: React.FC = () => {
       <Route component={SignIn} path="/" exact />
       <Route component={SignUp} path="/signup" />
       <Route component={ForgotPasswrod} path="/forgot-password" />
+      <Route component={ResetPassword} path="/reset-password" />
 
       <Route path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
