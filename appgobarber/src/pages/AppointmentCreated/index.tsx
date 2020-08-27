34b39b939e2fe3fe795bbba73ce74995dc115fa1
Gabriel Cunha <gn_cunha@hotmail.com>
2020-08-27 15:41:52 -0300

    Crianda Páginas e Rotas

---
+++ appgobarber/src/pages/AppointmentCreated/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { View, Button } from 'react-native';
+
+const AppointmentCreated: React.FC = () => <View />;
+
+export default AppointmentCreated;
---
+++ appgobarber/src/pages/CreateAppointment/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { View, Button } from 'react-native';
+
+const CreateAppointment: React.FC = () => <View />;
+
+export default CreateAppointment;
